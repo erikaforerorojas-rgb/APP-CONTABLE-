8c5913c646c1221662b5ce2c5dfc4a086d70a4ae
erikaforerorojas-rgb <erikaforerorojas@gmail.com>
2026-05-04 16:17:20 -0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,30 @@
-self.addEventListener("install", () => {
+const CACHE_NAME = "app-contable-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+// INSTALAR
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
   self.skipWaiting();
 });
 
+// ACTIVAR
 self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener("fetch", () => {});
+// FETCH (offline first)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
